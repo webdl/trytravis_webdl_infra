fc6ba175c47d471c52636f21a2d23c36402b3722
Тимофеев Кирилл <tk@webdl.ru>
2018-10-23 01:22:49 +0300

    Add script for create reddit instance

---
+++ config-scripts/create-redditvm.sh
@@ -0,0 +1,7 @@
+gcloud compute instances create reddit-app \
+--boot-disk-size=10GB \
+--image-family reddit-full \
+--machine-type=g1-small \
+--tags puma-server \
+--restart-on-failure \
+--zone europe-west4-a
