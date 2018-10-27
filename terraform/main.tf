be31484e950e6388c8c79260bbb3474816502b14
Кирилл Тимофеев <tk@webdl.ru>
2018-10-27 20:13:47 +0300

    Completed Homework #6, without asterisk job

---
+++ terraform/main.tf
@@ -0,0 +1,67 @@
+provider "google" {
+  version = "1.4.0"
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["reddit-app"]
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  # Определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  # Определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоеденить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из интернета
+    access_config {}
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = "false"
+    private_key = "${file("~/.ssh/id_rsa")}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешить доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с перечисленными тегами
+  target_tags = ["reddit-app"]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west4"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable "private_key_path" {
+  description = "Path to the private key used for provisioner connection"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable zone {
+  description = "Zone"
+  default     = "europe-west4-a"
+}
